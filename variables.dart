73f6a233c70d22d900cecb34089e83840d557628
Ahuja <rcy007@gmail.com>
2025-01-02 03:38:20 +0530

    Added variables!

--- variables.dart
+++ variables.dart
@@ -1,3 +1,22 @@
 void main() {
   var name = 'Bob';
+  Object name2 = 'Bob';
+  String name3 = 'Bob';
+  // int? lineCount;
+  // assert(lineCount == null);
+
+  int lineCount;
+
+  var a = 0;
+  var b = ++a;
+  print(b);
+  String playerName(String? name) => name ?? 'Guest';
+  print(playerName('Jack'));
+  // if (name != 'Bob') {
+  //   lineCount = 1;
+  // } else {
+  //   lineCount = 0;
+  // }
+
+  // print(lineCount);
 }
